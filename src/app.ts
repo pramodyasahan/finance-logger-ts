da0e5b7e79dc7ebe86b52e4b85d3950c26fa0c27
pramodyasahan <pramodyasahan.edu@gmail.com>
2025-04-13 14:58:47 +0530

    feat: add DOM elements

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import {Invoice} from "./models/invoice";
+
+
+let invoices: Invoice[] = []
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        details.value,
+        tofrom.value,
+        amount.value,
+    );
+})
