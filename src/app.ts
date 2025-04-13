04d90af01317a3973c402b5bc9c4926a02ff1e98
pramodyasahan <pramodyasahan.edu@gmail.com>
2025-04-13 15:45:45 +0530

    feat: render the objects has been created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import {Invoice} from "./models/Invoice";
-import {Payment} from "./models/Payment";
-import {HasFormatter} from "./interfaces/HasFormatter"
+import {Invoice} from "./models/Invoice.js";
+import {Payment} from "./models/Payment.js";
+import {HasFormatter} from "./interfaces/HasFormatter.js"
+import {ListTemplates} from "./models/ListTemplates.js";
 
 let invoices: Invoice[] = []
 
@@ -9,6 +10,9 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!
+const list = new ListTemplates(ul)
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -21,5 +25,6 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log(doc.format);
+    list.render(doc, type.value, 'end')
+
 })
