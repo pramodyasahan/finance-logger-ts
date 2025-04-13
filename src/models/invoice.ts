2947fb8cc03415df754da4b0be3e29e40e7d93ef
pramodyasahan <pramodyasahan.edu@gmail.com>
2025-04-13 15:07:56 +0530

    feat: add interface to the class

--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -1,4 +1,7 @@
-export class Invoice {
+import {HasFormatter} from "../interfaces/HasFormatter";
+
+
+export class Invoice implements HasFormatter {
     client: string;
     details: string;
     amount: number;
